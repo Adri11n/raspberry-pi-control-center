a5f033cee4fe619a7ba1be9e3dba3a7e50d72fcd
Adri11n <76658955+Adri11n@users.noreply.github.com>
2021-04-10 22:24:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
 apt install python3 python3-pip gunicorn
-<<<<<<< HEAD
 pip install Flask
 pip install Flask-HTTPAuth
-=======
-pip3 install Flask
-pip3 install Flask-HTTPAuth
->>>>>>> 3a2414d0ee53e2b780de6d01e5a75fb62a3a5613
