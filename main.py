dec317105d77ecc6ad73cddf58fd8e4ba63fcd36
Adri11n <adriaanvanvliet2005@gmail.com>
2021-05-03 22:37:00 +0200

    switched too psutils

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_httpauth import HTTPDigestAuth
 import os
 import json
 import psutil
+import datetime
 with open("settings.json", "r") as f_in:
     info = json.load(f_in)
 app = Flask(__name__)
@@ -16,13 +17,10 @@ def index():
     with open("english.lang.json") as conf:
         language = json.load(conf)
 
-    temp = psutil.sensors_temperatures()["coretemp"][0][1]
+    temp = psutil.sensors_temperatures(["coretemp"][0][1])
     clock = round(psutil.cpu_freq()[0])
-    cpuusage = os.popen("vmstat | tail -1 | awk '{print $15}'").read()
-    cpuusage = 100 - int(cpuusage)
-    uptime_row = os.popen("uptime").read().split("up")
-    uptime = uptime_row[1].split(",")
-    uptime = uptime[0] + "," + uptime[1]
+    cpuusage = psutil.cpu_percent(interval=1, percpu=False)
+    uptime = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%S:%M:%H %d-%m-%Y")
 
     return render_template("index.html", TXT_CONFIRM= language["TXT_CONFIRM"], TXT_RUNTIME=language["TXT_RUNTIME"], uptime_finally=uptime, temperatur=temp, TXT_TEMPERATURE=language["TXT_TEMPERATURE"], cpuusage_final=cpuusage, TXT_USAGE=language["TXT_USAGE"], TXT_CLOCK=language["TXT_CLOCK"], clock_final=clock, TXT_RESTART_2=language["TXT_RESTART_2"], TXT_SHUTDOWN_2=language["TXT_SHUTDOWN_2"], TXT_SHUTDOWN_1=language["TXT_SHUTDOWN_1"], TXT_RESTART_1=language["TXT_RESTART_1"])
 @app.route("/shutdown/<wich>")
